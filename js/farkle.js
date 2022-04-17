becda5e900711562df69056a94d388a873d20522
Cory Buckley <corybuckley@gmail.com>
2022-04-17 09:05:04 -0700

    Various updates
    Change button colors, add remaining score calculator, add audio, allow score keeper to change active player by tapping on player score row

--- js/farkle.js
+++ js/farkle.js
@@ -1,5 +1,19 @@
 var Keyboard = window.SimpleKeyboard.default;
 
+var ouchAudio = new Audio('audio/ouch.mp3');
+var updateAudio = new Audio('audio/update.wav');
+var wipeAudio = new Audio('audio/wipe.wav');
+ouchAudio.playbackRate = 1.75;
+
+function resetAudio() {
+  ouchAudio.pause();
+  updateAudio.pause();
+  wipeAudio.pause();
+  ouchAudio.currentTime = 0;
+  updateAudio.currentTime = 0;
+  wipeAudio.currentTime = 0;
+}
+
 var winTarget = 10000;
 var workingNumber = '0';
 var players = [
@@ -20,19 +34,24 @@ function changeActivePlayer(index) {
 
   players.forEach(player => {
     document.getElementById(player.id).classList.remove('active-player');
-    document.getElementById(player.id).classList.remove('animate__animated', 'animate__pulse');
+    document.getElementById(player.id).classList.remove('animate__animated', 'animate__pulse', 'animate__faster');
   });
 
   document.getElementById(players[activePlayer].id).classList.add('active-player');
-  document.getElementById(players[activePlayer].id).classList.add('animate__animated', 'animate__pulse');
+  document.getElementById(players[activePlayer].id).classList.add('animate__animated', 'animate__pulse', 'animate__faster');
+  workingNumber = 0;
+  document.getElementsByClassName('score-calculator-working-number')[0].innerHTML = workingNumber;
+  textFit(document.getElementsByClassName('score-calculator-working-number'));
 }
 
 function updateScores(){
   players.forEach(player => {
-    document.querySelectorAll('#' + player.id + ' .player-container-row-player-details-score')[0].innerHTML = player.score;
+    document.querySelectorAll('#' + player.id + ' .player-container-row-player-details-score')[0].innerHTML = player.score + '&#32;<span class="remaining-score">&nbsp;/&nbsp;' + (winTarget - player.score) + '</span>';
     var percentage = (player.score / winTarget) * 100;
     document.getElementById(player.id).style.background = 'linear-gradient(90deg, rgba(171,0,255,1) ' + percentage + '%, rgba(255,255,255,1) ' + percentage + '%)';
   });
+  resetAudio();
+  updateAudio.play();
 }
 
 function onKeyPress(button) {
@@ -61,6 +80,11 @@ function onKeyPress(button) {
       updateScores();
       changeActivePlayer();
       break;
+    case 'Farkle':
+      changeActivePlayer();
+      resetAudio();
+      ouchAudio.play();
+      break;
     default:
       if(workingNumber == '0') {
         workingNumber = button;
@@ -72,17 +96,23 @@ function onKeyPress(button) {
       }
       break;
   }
-  textFit(document.getElementsByClassName('score-calculator-working-number')); 
+  textFit(document.getElementsByClassName('score-calculator-working-number'));
 }
 
 var keyboard = new Keyboard({
   onKeyPress: button => onKeyPress(button),
   layout: {
-    default: ['1 2 3', '4 5 6', '7 8 9', 'Clear 0 Backspace', 'Subtract Add'],
+    default: ['1 2 3', '4 5 6', '7 8 9', 'Clear 0 Backspace', 'Subtract Add Farkle'],
   },
+  buttonTheme : [
+    {
+      class: 'dark-button',
+      buttons: 'Clear Backspace Subtract Add Farkle'
+    }
+  ],
   theme: "hg-theme-default hg-layout-numeric numeric-theme"
 });
 
-textFit(document.getElementsByClassName('score-calculator-working-number'));
 changeActivePlayer(0);
 updateScores();
+textFit(document.getElementsByClassName('score-calculator-working-number'));
